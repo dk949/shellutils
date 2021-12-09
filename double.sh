57ea6f245c9fe56e08be4f0ec173341407d12a94
dk949 <56653556+dk949@users.noreply.github.com>
2021-12-09 22:16:31 +0000

    added some comments and removed a print

--- double.sh
+++ double.sh
@@ -10,13 +10,12 @@ if ! [ -d "$1" ]; then
 fi
 
 
-TPID="$(ps -o ppid= $PPID | tr -d ' ')" # parnt of a parent of a parent of a parent.
-T="$(awk '{print $2}' "/proc/$TPID/stat" | sed 's/(\|)//g')"
+TPID="$(ps -o ppid= $PPID | tr -d ' ')" # Terminal PID
+T="$(awk '{print $2}' "/proc/$TPID/stat" | sed 's/(\|)//g')" # Terminal name
 
 set +m
 CMD="sh -c cd $1;$T"
 
-printf "TPID = %s, T= %s, CMD = %s" "$TPID" "$T" "$CMD"
 
 eval nohup "$CMD" 1>/dev/null 2>/dev/null </dev/null &
 eval nohup "$CMD" 1>/dev/null 2>/dev/null </dev/null &
