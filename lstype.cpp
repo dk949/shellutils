0fdb3315253461ac7f65f9c85b99f9ee153fed98
dk949 <56653556+dk949@users.noreply.github.com>
2022-01-05 13:21:52 +0200

    cleaned up lstype

--- lstype.cpp
+++ lstype.cpp
@@ -5,16 +5,16 @@
 #include <string_view>
 namespace fs = std::filesystem;
 
+#ifndef VERSION
+#define VERSION "unknonw"
+#endif
+
 std::optional<fs::path> getShabang(const fs::path path) {
     auto f = std::ifstream(path);
     std::string shebang;
     std::getline(f, shebang);
-    if (shebang.size() < 4) {  // assuming at least 4 characters "#", "!", "/" and name of the interpreter
+    if (!f.good() || shebang.size() < 3 || std::string_view {std::begin(shebang), std::begin(shebang) + 2} != "#!")
         return std::nullopt;
-    }
-    if (std::string_view {std::begin(shebang), std::begin(shebang) + 3} != "#!/") {
-        return std::nullopt;
-    }
     return fs::path {std::begin(shebang) + 3, std::end(shebang)};
 }
 
@@ -23,24 +23,16 @@ void print(const char *type, const fs::path &path) {
 }
 
 int main(int argc, const char **argv) {
-    if (argc == 2) {
-        if (std::string_view(argv[1]) == "-v") {
+    if (argc != 1) {
+        if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'v') {
             std::cout << argv[0] << ": v" VERSION << '\n';
             return 0;
         }
-        if (std::string_view(argv[1]) == "-h") {
-            std::cout << "Usage: " << argv[0] << " -hv\n";
-            return 0;
-        }
-        std::cerr << "Usage: " << argv[0] << " -hv\n";
+        std::cerr << "Usage: " << argv[0] << " -h -v\n";
         return 1;
     }
-    if (argc > 2) {
-        std::cerr << "Usage: " << argv[0] << " -hv\n";
-        return 1;
-    }
-    const char *path = ".";
-    for (const auto entry : fs::directory_iterator(path)) {
+
+    for (const auto &entry : fs::directory_iterator(".")) {
         if (entry.is_regular_file()) {
             if (const auto shebang = getShabang(entry.path())) {
                 print(shebang->filename().c_str(), entry.path().filename());
