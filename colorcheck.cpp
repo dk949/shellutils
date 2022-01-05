91a404545455a5dd4a7f9d2e21eca1377607e50f
dk949 <56653556+dk949@users.noreply.github.com>
2022-01-05 13:13:09 +0200

    improved color checker

--- colorcheck.cpp
+++ colorcheck.cpp
@@ -1,35 +1,58 @@
+#include <array>
 #include <cstring>
 #include <iostream>
+#include <stdlib.h>
+
+#ifndef VERSION
+#define VERSION "unknonw"
+#endif
+
+constexpr std::array toStr = []() {
+    std::array<char[3], 256> ret;
+    for (int i = 0; i < 256; i++) {
+        ret[i][0] = (i / 100) + '0';
+        ret[i][1] = (i % 100) / 10 + '0';
+        ret[i][2] = (i % 10) + '0';
+    }
+    return ret;
+}();
+
+constinit std::array buf = []() {
+    constexpr auto emptyColor = "\x1b[48;2;000;000;000m \x1b[0m";
+    constexpr auto len = std::char_traits<char>::length(emptyColor);
+    std::array<char[len], 256> ret;
+    for (int b = 0; b < 256; b++) {
+        std::char_traits<char>::copy(ret[b], emptyColor, len);
+        ret[b][15] = toStr[b][0];
+        ret[b][16] = toStr[b][1];
+        ret[b][17] = toStr[b][2];
+    }
+    return ret;
+}();
+
 int main(int argc, const char **argv) {
-    if (argc == 2) {
-        if (std::strcmp(argv[1], "-v") == 0) {
+    if (argc != 1) {
+        if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 'v') {
             std::cout << argv[0] << ": v" VERSION << '\n';
             return 0;
         }
-        if (std::strcmp(argv[1], "-h") == 0) {
-            std::cout << "Usage: " << argv[0] << " -hv\n";
-            return 0;
-        }
-        std::cerr << "Usage: " << argv[0] << " -hv\n";
-        return 1;
-    }
-    if (argc > 2) {
-        std::cerr << "Usage: " << argv[0] << " -hv\n";
+        std::cerr << "Usage: " << argv[0] << " -h -v\n";
         return 1;
     }
 
+    for (int r = 0; r < 256; r++) {
+        for (int g = 0; g < 256; g++) {
+            for (int b = 0; b < 256; b++) {
+                buf[b][7] = toStr[r][0];
+                buf[b][8] = toStr[r][1];
+                buf[b][9] = toStr[r][2];
 
-    for (int r = 0; r < 32; r++) {
-        for (int g = 0; g < 32; g++) {
-            for (int b = 0; b < 32; b++) {
-                std::cout            //
-                    << "\x1b[48;2;"  //
-                    << r * 8 << ';'  //
-                    << g * 8 << ';'  //
-                    << b * 8         //
-                    << "m \x1b[0m";  //
+                buf[b][11] = toStr[g][0];
+                buf[b][12] = toStr[g][1];
+                buf[b][13] = toStr[g][2];
             }
+            std::fwrite(buf.data(), sizeof(char), sizeof(buf), stdout);
         }
     }
-    std::cout << '\n';
+    std::putc('\n', stdout);
 }
