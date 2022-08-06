e30a861d940bad4c756c3f53bdda939518ffd312
dk949 <56653556+dk949@users.noreply.github.com>
2022-08-06 14:00:10 +0300

    fixed not being able to copy directories with spaces

--- cpdir.sh
+++ cpdir.sh
@@ -3,5 +3,14 @@ if [ $# -ne 2 ]; then
     echo "Usage: $(basename "$0") DIR DEST"
     exit 1
 fi
-cd "$2" || exit 1
-find "$1" -type d -exec realpath --relative-to "$1" {} \; | grep -vx "\.git\(/.*\)\?\|\." | xargs mkdir || exit 1
+
+DIR="$1"
+DEST="$2"
+
+DIRLIST=$(find "$DIR" -type d -exec realpath --relative-to "$DIR" {} \; | grep -vx "\.git\(/.*\)\?\|\.")
+
+cd "$DEST" || exit 1
+
+IFS="
+"
+mkdir $DIRLIST
