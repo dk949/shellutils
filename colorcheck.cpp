9558ed96c929481432e875d71afc5afbb384bbd8
dk949 <56653556+dk949@users.noreply.github.com>
2024-01-23 14:40:08 +0000

    calng tidied colorcheck

--- colorcheck.cpp
+++ colorcheck.cpp
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-
 #include <array>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 
@@ -8,8 +7,8 @@
 #    define VERSION "unknonw"
 #endif
 
-constexpr std::array toStr = []() {
-    std::array<char[3], 256> ret;
+constexpr auto to_str = []() {
+    std::array<char[3], 256> ret {};
     for (int i = 0; i < 256; i++) {
         ret[i][0] = (i / 100) + '0';
         ret[i][1] = (i % 100) / 10 + '0';
@@ -18,15 +17,15 @@ constexpr std::array toStr = []() {
     return ret;
 }();
 
-constinit std::array buf = []() {
-    constexpr auto emptyColor = "\x1b[48;2;000;000;000m \x1b[0m";
-    constexpr auto len = std::char_traits<char>::length(emptyColor);
-    std::array<char[len], 256> ret;
+static constinit auto buf = []() {
+    constexpr auto empty_color = "\x1b[48;2;000;000;000m \x1b[0m";
+    constexpr auto len = std::char_traits<char>::length(empty_color);
+    std::array<char[len], 256> ret {};
     for (int b = 0; b < 256; b++) {
-        std::char_traits<char>::copy(ret[b], emptyColor, len);
-        ret[b][15] = toStr[b][0];
-        ret[b][16] = toStr[b][1];
-        ret[b][17] = toStr[b][2];
+        std::char_traits<char>::copy(ret[b], empty_color, len);
+        ret[b][15] = to_str[b][0];
+        ret[b][16] = to_str[b][1];
+        ret[b][17] = to_str[b][2];
     }
     return ret;
 }();
@@ -44,13 +43,13 @@ int main(int argc, char const **argv) {
     for (int r = 0; r < 256; r++) {
         for (int g = 0; g < 256; g++) {
             for (int b = 0; b < 256; b++) {
-                buf[b][7] = toStr[r][0];
-                buf[b][8] = toStr[r][1];
-                buf[b][9] = toStr[r][2];
+                buf[b][7] = to_str[r][0];
+                buf[b][8] = to_str[r][1];
+                buf[b][9] = to_str[r][2];
 
-                buf[b][11] = toStr[g][0];
-                buf[b][12] = toStr[g][1];
-                buf[b][13] = toStr[g][2];
+                buf[b][11] = to_str[g][0];
+                buf[b][12] = to_str[g][1];
+                buf[b][13] = to_str[g][2];
             }
             std::fwrite(buf.data(), sizeof(char), sizeof(buf), stdout);
         }
