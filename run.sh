51a852478bef04a714a3d50e2c95fed56e389074
dk949 <56653556+dk949@users.noreply.github.com>
2024-04-16 12:32:06 +0100

    deprecate run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-echo "run is deprecated and will soon be removed"
-if [ $# -ne 1 ];then
-    echo "Usage: $(basename "$0") PROGRAM"
-    exit 1
-fi
-eval nohup "$1" 1>/dev/null 2>/dev/null </dev/null  &
-kill "$(awk '{print $4}' "/proc/$PPID/stat")"
-
+echo "run has been deprecated just use exec"
+exit 1
