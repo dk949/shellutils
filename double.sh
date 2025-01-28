63ad1f55003ac2a04d89106b996dde7f112a0cec
dk949 <56653556+dk949@users.noreply.github.com>
2025-01-28 15:18:34 +0000

    replaced double with gpath

--- double.sh
+++ double.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "Usage $(basename $0) DIR"
-    exit 1
-fi
-if ! [ -d "$1" ]; then
-    echo "$1 is not a directory"
-    exit 1
-fi
 
-
-TPID="$(ps -o ppid= $PPID | tr -d ' ')" # Terminal PID
-T="$(awk '{print $2}' "/proc/$TPID/stat" | sed 's/(\|)//g')" # Terminal name
-
-set +m
-CMD="sh -c cd $1;$T"
-
-
-eval nohup "$CMD" 1>/dev/null 2>/dev/null </dev/null &
-eval nohup "$CMD" 1>/dev/null 2>/dev/null </dev/null &
-
-kill "$TPID"
+echo "Deprecated. Use gpath && gpath -k to replecate old behaviour"
+exit 1
---
+++ gpath.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+help(){
+    echo "Usage: $(basename "$0") [DIR] [OPTIONS]
+
+    Open another copy of the current terminal emulator.
+
+    DIR                     Directory to open the new terminal in
+
+    -h,--help               Show this message and exit
+    -k,--kill               Kill the original terminal
+"
+    exit
+}
+
+dir="$PWD"
+while [ $# -gt 0 ]; do
+case $1 in
+    -k|--kill)
+        kill=1
+        shift
+        ;;
+    -h|--help)
+        help
+        ;;
+    *)
+        dir=$1
+        shift
+        ;;
+esac
+done
+
+
+tpid="$(ps -o ppid= $PPID | tr -d ' ')" # Terminal PID
+tcmd="$(ps -p "$tpid" -o command=)" # terminal command
+
+
+set +m
+cmd="sh -c cd $dir;$tcmd"
+
+eval nohup "$cmd" 1>/dev/null 2>/dev/null </dev/null &
+[ -n "$kill" ] && kill "$tpid"
