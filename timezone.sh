06a75867eacc42667f3fd331a4fcfe9f9b0c032c
dk949 <56653556+dk949@users.noreply.github.com>
2026-05-09 21:49:46 +0100

    make timezon support cities with spaces

--- timezone.sh
+++ timezone.sh
@@ -19,7 +19,9 @@ command -v timedatectl >/dev/null || die 1 "This script uses timedatectl"
 #shellcheck disable=SC2016 # I don't want the backticks to expand
 [ "$(id -u)" = 0 ] && echo 'WARNING: Do not execute the script as root. `timedatectl` will prompt for authentication if needed.'
 city=$1
-search_result=$(timedatectl list-timezones | grep "$city" -i) || die 1 "Could not find any matches for city '$city'"
+city_san=$(echo "$city" | tr ' ' '_')
+
+search_result=$(timedatectl list-timezones | grep "$city_san" -i) || die 1 "Could not find any matches for city '$city'"
 [ "$(echo "$search_result" | wc -l)" -gt 1 ] && die 1 "Found multiple results for city '$city'. Choose one of the following:
 $search_result"
 echo setting timezone to "$search_result"
